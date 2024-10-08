aac2312a63b10777e7327c481185c9135d249d38
dennoko <sd514dennoko@gmail.com>
2024-10-08 18:09:52 +0900

    ViewModelを作成

---
+++ app/src/main/java/com/example/tutorialcounterapp/counter_screen/CounterScreenViewModel.kt
@@ -0,0 +1,15 @@
+package com.example.tutorialcounterapp.counter_screen
+
+import androidx.lifecycle.ViewModel
+
+class CounterScreenViewModel: ViewModel() {
+
+
+    // TODO: fun saveItems(items: Set<String>) の実装(アイテム名のリストを保存する)
+
+
+    // TODO: fun saveCountValue(itemName: String, countValue: Int) の実装(アイテム名をキーとしてカウント値を保存する)
+
+
+    // TODO: fun loadCountValue(itemName: String): Int の実装(アイテム名をキーとして保存されたカウント値を取得する)
+}
